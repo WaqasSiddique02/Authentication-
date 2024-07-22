2c90deba87c86ef3fe845e82ba176ee14e4c0730
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2024-07-22 15:10:51 +0500

    Added hashing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const ejs = require('ejs');
 const mongoose=require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5=require("md5");
+
 const app = express();
 
 app.use(express.static('public'));
@@ -15,9 +16,6 @@ const userSchema=new mongoose.Schema({
     email:String,
     password:String
 });
-
-
-userSchema.plugin(encrypt,{secret:process.env.secret,encryptedFields:['password']});
 
 const User= new mongoose.model("User",userSchema);
 
@@ -36,7 +34,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     newUser.save()
@@ -50,7 +48,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const userName=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
 
     User.findOne({email:userName})
     .then(function(foundUser){
